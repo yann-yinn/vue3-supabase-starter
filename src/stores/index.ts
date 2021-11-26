7006cbaefd3e26d014da9da1b47186affc293350
yann <yann.boisselier@gmail.com>
2021-11-27 00:21:13 +0100

    pinia user state: clean sync with local storage

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,7 +1,28 @@
 import { defineStore } from "pinia";
 import { User } from "@/types";
 
-function userFromLocalStorage() {
+export const useStore = defineStore("main", {
+  state: () => {
+    return {
+      user: getLocalStorageUser(),
+    };
+  },
+});
+
+useStore().$subscribe((mutation, state) => {
+  setLocalStorageUser(state.user);
+});
+
+function setLocalStorageUser(user: User | null) {
+  // save user object to local storage, if different from null
+  if (user === null) {
+    localStorage.removeItem("user");
+  } else {
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+}
+
+function getLocalStorageUser() {
   let user: null | User = null;
   const userJsonString = localStorage.getItem("user");
   if (userJsonString) {
@@ -9,23 +30,3 @@ function userFromLocalStorage() {
   }
   return user;
 }
-
-// useStore could be anything like useUser, useCart
-// the first argument is a unique id of the store across your application
-export const useStore = defineStore("main", {
-  // arrow function recommended for full type inference
-  state: () => {
-    return {
-      user: userFromLocalStorage(),
-    };
-  },
-});
-
-useStore().$subscribe((mutation, state) => {
-  // save user object to local storage, if different from null
-  if (state.user === null) {
-    localStorage.removeItem("user");
-  } else {
-    localStorage.setItem("user", JSON.stringify(state.user));
-  }
-});
